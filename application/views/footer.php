5f7362c716403608ad8a8c0ebeedc19991303d78
Abhinav Raj <abhinav.raj0509@gmail.com>
2024-10-08 17:27:06 +0530

    initial setup

---
+++ application/views/footer.php
@@ -0,0 +1,168 @@
+ <!-- Footer Area -->
+
+ <footer class="footer-area">
+        <div class="container">
+            <div class="footer-up">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-12">
+                        <a href="index.html" class="logo">Saka India.</a>
+                        <p>Design your Dream House <br> maxime dolor voluptatibus natus
+                        </p>
+                    </div>
+                    <div class="col-lg-2 col-md-6">
+                        <h5>Office</h5>
+                        <p>Corporate Office- <br>
+                            175 24th Street, DW-1 3567
+                            London, UK 265
+                        </p>
+                        <div class="company-email">
+                            <a href="#">info@Saka India.com</a>
+                        </div>
+                        <div class="phone-number">
+                            <p>+18-4675826</p>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-2 offset-lg-1 col-md-6 com-sm-12">
+                        <h5>Links</h5>
+                        <ul>
+                            <li>
+                                <a href="about.html">About</a>
+                                <a href="services.html">Services</a>
+                                <a href="project.html">Project</a>
+                                <a href="price.html">Pricing</a>
+                                <a href="faq.html"> FAQ</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12">
+                        <h5>Get in Touch</h5>
+                        <ul>
+                            <li>
+                                <div class="social-area">
+                                    <a href="#"><i class="lab la-facebook-f"></i>Facebook</a>
+                                    <a href="#"><i class="lab la-instagram"></i>Instagram</a>
+                                    <a href="#"><i class="lab la-linkedin-in"></i>linkedin</a>
+                                    <a href="#"><i class="la la-skype"></i>Skype</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Footer Bottom Area -->
+
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row justify-content-center align-items-center justify-content-center">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <p class="copyright-line">© 2024 Saka India. All rights reserved.</p>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 text-md-end">
+                    <p class="privacy">Privacy Policy | Terms &amp; Conditions</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+    </div>
+
+    <!-- Search Dropdown Area -->
+
+    <div class="search-popup">
+        <span class="search-back-drop"></span>
+
+        <div class="search-inner">
+            <div class="container">                
+                <button class="close-search"><span class="la la-times"></span></button>                
+                <form method="post" action="index.html">
+                    <div class="form-group">
+                        <input type="search" name="search-field" value="" placeholder="Type your keyword" required="">
+                        <button type="submit"><i class="fal fa-search"></i></button>                        
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Off-canvas Area-->
+
+    <div class="extra-info">
+        <div class="close-icon menu-close">
+            <button>
+                <i class="las la-times"></i>
+            </button>
+        </div>
+        <div class="logo-side mb-30">
+            <div class="logo">
+                <a href="index-2.html" class="logo">Saka India.</a>
+            </div>
+        </div>
+        <div class="side-info">
+            <div class="contact-list mb-40">
+                <!-- <h4>About</h4> -->
+                <p>Welcome to Saka India, a full service architecture and interior design firm. We specialize in creating beautiful, functional spaces that reflect your unique style.</p>
+                <img src="assets/img/offcanvas-img.jpg" alt="">
+
+                <div class="mt-30 mb-30">
+                    <a href="contact.html" class="white-btn">Get In Touch</a>
+                </div>
+            </div>
+            <div class="social-area-wrap">
+                <a href="#"><i class="lab la-facebook-f"></i></a>                                    
+                <a href="#"><i class="lab la-instagram"></i></a>
+                <a href="#"><i class="lab la-linkedin-in"></i></a>
+                <a href="#"><i class="lab la-skype"></i></a>                
+            </div>
+        </div>
+    </div>
+
+    <div class="offcanvas-overlay"></div>
+
+
+    <!-- back to top start -->
+    <div class="progress-wrap">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+    </div>
+
+
+    <!-- Popper JS -->
+    <script src="<?php echo base_url('assets/js/popper.min.js')?>"></script>
+    <!-- Bootstrap JS -->
+    <script src="<?php echo base_url('assets/js/bootstrap.min.js')?>"></script>
+    <!-- Wow JS -->
+    <script src="<?php echo base_url('assets/js/wow.min.js')?>"></script>
+    <!-- Way Points JS -->
+    <script src="<?php echo base_url('assets/js/jquery.waypoints.min.js')?>"></script>
+    <!-- Counter Up JS -->
+    <script src="<?php echo base_url('assets/js/jquery.counterup.min.js')?>"></script>
+    <!-- Owl Carousel JS -->
+    <script src="<?php echo base_url('assets/js/owl.carousel.min.js')?>"></script>
+    <!-- Slick Slider JS -->
+    <script src="<?php echo base_url('assets/js/slick.min.js')?>"></script>        
+    <!-- Back To Top JS -->
+    <script src="<?php echo base_url('assets/js/backToTop.js')?>"></script>
+    <!-- Metis Menu JS -->
+    <script src="<?php echo base_url('assets/js/metismenu.js')?>"></script>
+    <!-- GSAP Animation JS -->
+    <script src="<?php echo base_url('assets/js/gsap.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/gsap-scroll-to-plugin.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/SplitText.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/ScrollSmoother.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/ScrollTrigger.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/smoother-script.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/heading-animation.js')?>"></script>
+    <script src="<?php echo base_url('assets/js/paragraph-animation.js')?>"></script>
+    <!-- Main JS -->
+    <script src="<?php echo base_url('assets/js/main.js')?>"></script>
+
+</body>
+
+</html>
