ef7bdef0294a345a278bfb4650f4282e90e25425
Abhinav Raj <abhinav.raj0509@gmail.com>
2025-02-20 15:30:40 +0530

    completed clients page

--- application/views/NFront/clients.php
+++ application/views/NFront/clients.php
@@ -69,45 +69,93 @@
 
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                    <img src="<?php echo base_url();?>assets/img/client/aarti-logo.png" alt="aarti-logo">
+                    <img src="<?php echo base_url();?>assets/img/client/Chem-Process-Systems-logo.png" alt="Chem-Process-System-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                    <img src="<?php echo base_url();?>assets/img/client/aarti-logo.png" alt="aarti-logo">
+                    <img src="<?php echo base_url();?>assets/img/client/JOHNSON-logo.png" alt="Johnson-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                    <img src="<?php echo base_url();?>assets/img/client/aarti-logo.png" alt="aarti-logo">
+                    <img src="<?php echo base_url();?>assets/img/client/India-Pesticides-Limited.png" alt="India-Pesticides-Logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                    <img src="<?php echo base_url();?>assets/img/client/aarti-logo.png" alt="aarti-logo">
+                    <img src="<?php echo base_url();?>assets/img/client/asianpaints-logo.png" alt="HG-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                    <img src="<?php echo base_url();?>assets/img/client/aarti-logo.png" alt="aarti-logo">
+                    <img src="<?php echo base_url();?>assets/img/client/HG-logo.png" alt="HG-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                    <img src="<?php echo base_url();?>assets/img/client/aarti-logo.png" alt="aarti-logo">
+                    <img src="<?php echo base_url();?>assets/img/client/wbeil-logo.png" alt="wbeil-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                    <img src="<?php echo base_url();?>assets/img/client/aarti-logo.png" alt="aarti-logo">
+                    <img src="<?php echo base_url();?>assets/img/client/albenion-clean-energy-logo.png" alt="HG-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                        <img src="http://fakeimg.pl/150x150/" class="img-responsive">
+                        <img src="<?php echo base_url();?>assets/img/client/UPL-logo.png" alt="UPL-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                        <img src="http://fakeimg.pl/150x150/" class="img-responsive">
+                     <img src="<?php echo base_url();?>assets/img/client/Travancore-Cochin-Chemicals.png" alt="Travancore-Cochin-Chemicals-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                        <img src="http://fakeimg.pl/150x150/" class="img-responsive">
+                     <img src="<?php echo base_url();?>assets/img/client/BARC-logo.png" alt="BAARC-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                        <img src="http://fakeimg.pl/150x150/" class="img-responsive">
+                    <img src="<?php echo base_url();?>assets/img/client/coromandel-logo.png" alt="coromandel-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                        <img src="http://fakeimg.pl/150x150/" class="img-responsive">
+                    <img src="<?php echo base_url();?>assets/img/client/Kiri-Industries-logo.png" alt="Kiri-Industries-logo">
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
-                        <img src="http://fakeimg.pl/150x150/" class="img-responsive">
+                       <img src="<?php echo base_url();?>assets/img/client/Thermax-logo.png" alt="Thermax-logo">
+
                     </div>
-                    
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/Shree-logo.png" alt="Shree-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/Avesta-logo.png" alt="Avesta-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/A-logo.png" alt="A-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/Bhagera-logo.png" alt="Bhagera-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/Indofil-logo.png" alt="Indofil-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/Lonsen-Kiri-logo.png" alt="Lonsen-Kiri-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/SAIPL-logo.png" alt="SAIPL-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/THIRUMALAI-Chemicals-logo.png" alt="THIRUMALAI-Chemicals-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/Aditya-Birla-Group-logo.png" alt="SAIPL-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/Clean-Science-logo.png" alt="Clean-Science-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/Jesons-logo.png" alt="Jesons-logo">
+
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 mt-40">
+                       <img src="<?php echo base_url();?>assets/img/client/Nikhil-Adhesive-logo.png" alt="Nikhil-Adhesive-logo">
+
+                    </div>
                     
 
 
@@ -211,21 +259,30 @@
      </div>
     <!-- Testimonial Section  -->
 
-    <div class="testimonial-section section-padding pt-0">
+    <div class="testimonial-section section-padding pt-50">
         <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section-title mb-0 text-md-end">
+                        <h3><i class="las la-arrow-down d-none d-md-block"></i> What Our Clients Are Saying</h3>
+                    </div>                    
+                </div>
+            </div>
+            <hr>
             <div class="row mt-50">
                 <div class="col-xl-7 col-lg-7">
                     <div class="testimonial-wrapper owl-carousel">
+                    
                         <div class="single-testimonial-item">
                             <div class="quote-icon">
                                 <img src="assets/img/straight-quotes.png" alt="">
                             </div>
                             <div class="testimonial-content">
-                                <p>I hired Saka India to design and build my dream home, and I could be happier with the result. From the intial consultation to the final walk-through, the Saka India team was professional.</p>
+                                <p>We’ve been working with Saka India for over three years now, and their commitment to quality and precision is unmatched. Their team has consistently delivered high-grade materials that meet our stringent standards.</p>
                             </div>
                             <div class="testimonial-author">
                                 <img src="assets/img/testimonial/1.png" alt="">
-                                <h6 class="text-black">Lisa Ray <span>New York, USA</span></h6>
+                                <h6 class="text-black">Madhav Patel <span>Ahemedabad, Gujrat</span></h6>
                             </div>
 
                         </div>
@@ -234,18 +291,17 @@
                                 <img src="assets/img/straight-quotes.png" alt="">
                             </div>
                             <div class="testimonial-content">
-                                <p>I hired Saka India to design and build my dream home, and I could be happier with the result. From the intial consultation to the final walk-through, the Saka India team was professional.</p>
+                                <p>Saka India has been instrumental in helping us streamline our manufacturing process. Their expertise in chemical processing and timely delivery has significantly reduced our downtime and improved efficiency</p>
                             </div>
                             <div class="testimonial-author">
                                 <img src="assets/img/testimonial/2.png" alt="">
                                 <h6 class="text-black">Paul Scholes <span>Manchester, UK</span></h6>
                             </div>
-
                         </div>
                     </div>
                 </div>
                 <div class="col-xl-5 col-lg-5">
-                    <div class="testimonial-bg-wrapper">
+                    <div class="testimonial-bg-wrapper d-none d-md-block">
                         <img src="assets/img/testimonial-bg.jpg" alt="">
                     </div>
                 </div>
